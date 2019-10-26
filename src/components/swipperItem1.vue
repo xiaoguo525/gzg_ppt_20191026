1392de8b4817d67b337a54387f4fcd5c9789044f
gzg <438797254@qq.com>
2019-10-26 17:03:10 +0800

    首次添加ppt

---
+++ src/components/swipperItem1.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="wrap">
+    电风扇
+  </div>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,110 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="wrap">
+		<div class="header">
+			<!-- 前端技术栈 -->
+		</div>
+		<swiper :options="swiperOption">
+			<swiper-slide>
+				<div class="item1">
+					<swipperItem1/>
+				</div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="item2">
+					2
+				</div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="item3">
+					3
+				</div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="item4">
+					4
+				</div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="item5">
+					5
+				</div>
+			</swiper-slide>
+			<div class="swiper-pagination" slot="pagination"></div>
+		</swiper>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+		name: 'carrousel',
+		components:{
+			swipperItem1:()=>import('@/components/swipperItem1')
+		},
+    data() {
+      return {
+        swiperOption: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+					direction:'vertical',
+					mousewheel: true,
+					paginationClickable: true,
+        },
+      }
+    },
+    mounted() {
+      // setInterval(() => {
+      //   console.log('simulate async data')
+      //   if (this.swiperSlides.length < 10) {
+      //     this.swiperSlides.push(this.swiperSlides.length + 1)
+      //   }
+      // }, 3000)
+    }
   }
-}
 </script>
+<style lang="scss">
+.wrap{
+	.header{
+		position: fixed;
+		width: 100%;
+		height: 60px;
+		line-height: 60px;
+		left: 0;
+		top: 0;
+		background-color: #007D6B;
+		z-index: 11;
+		border-bottom: 1px solid #999;
+	}
+	.swiper-container {
+		width: 100%;
+		height: 100vh;
+		
+		.swiper-slide{
+			&>div{
+				width: 100%;
+				height: 100%;
+				padding-top:60px;
+			}
+		}
+		.item1{
+			// background-color: #87B0A5;
+			background:url('../images/vue.jpg');
+			background-size: 100% 100%;
+			background-repeat:no-repeat;
+		}
+		.item2{
+			background-color: #007B5D;
+		}
+		.item3{
+			background-color: #4D5E8F;
+		}
+		.item4{
+			background-color: #4A839E;
+		}
+		.item5{
+			background-color: #8F5949;
+		}
+		
+	}  
+}
+</style>
